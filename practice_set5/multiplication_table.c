ce1699d5a9432e683009afbc1bf5f0c2eea58172
YOURNAME116 <adhikariutshav999@gmail.com>
2023-08-18 06:59:36 +0545

    multiplication table

--- practice_set5/multiplication_table.c
+++ practice_set5/multiplication_table.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
 int main() {
+int i,num;
+printf("Enter a number to print it's multiplication table: ");
+scanf("%d",&num);
+for(i=1;i<=10;i++){
+    printf("%d x %d = %d\n",num,i,(i*num));
+}
 
-printf("hello world");
  return 0;
 }
