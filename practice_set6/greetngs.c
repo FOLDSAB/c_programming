5f8ca38bd7f07ea1dd71c5680a5c4b1736fe3bf6
YOURNAME116 <adhikariutshav999@gmail.com>
2023-08-19 07:58:14 +0545

    greetings function calls

---
+++ practice_set6/greetngs.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void good_morning();
+void good_afternoon();
+void good_evening();
+int main() {
+good_morning();
+printf("\n");
+good_afternoon();
+printf("\n");
+good_evening();
+
+
+printf("\n");
+ return 0;
+}
+
+void good_morning(){
+    printf("good morning");
+}
+void good_afternoon(){
+    printf("good afternoon");
+}
+void good_evening(){
+    printf("good evening");
+}
